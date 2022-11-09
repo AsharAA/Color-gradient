d885e8122344042b6159c70c0caf7c67eca4db99
AD ASH <fashionest8@gmail.com>
2022-11-09 04:13:06 -0800

    publishing

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var _ = require('lodash');
+
+const arr = [1,2,3,4,5,6,7,8];
+console.log('Output:',_.without(arr,5));
+
 const css = document.querySelector("h3");
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector(".color2");
